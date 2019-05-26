9efb569afc56f875982281b759ce76beded5c0f1
Ariel J <arieljakubowski@gmail.com>
2019-05-26 01:28:40 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,6 +3,7 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, language|
     languages.each do |language, info|
+      binding.pry
       if new_hash[language] == nil
           new_hash[language] = info
         end
